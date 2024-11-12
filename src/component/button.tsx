eaf522dd766d61e0219bc3bc4d83a9eef70053aa
채종원 <cowhddnjsdl0@naver.com>
2024-11-12 14:48:57 +0900

    test

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   width?: number;
   buttonType?: "solid" | "outline";
+  test?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = ({
