fa82ba2a299ba50c7f4aac4f7c5941d7f1f2d2d2
채종원 <cowhddnjsdl0@naver.com>
2024-11-12 14:49:59 +0900

    안쓰는 props 삭제

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   width?: number;
   buttonType?: "solid" | "outline";
-  test?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = ({
